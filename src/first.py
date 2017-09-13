8b452d7b39ae8f0890bd9ebf24660be272a1e7d8
elisac <elisac@elisac-HP-255-G5-Notebook-PC>
2017-09-13 09:50:59 +0200

    File modificato

---
+++ src/first.py
@@ -0,0 +1,11 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = 3
+__date__ = "$12-set-2017 9.38.33$"
+
+if __author__ == "__main__":
+    print __author__
+else :
+    print "OMar"
